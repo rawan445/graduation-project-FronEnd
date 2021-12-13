e554727deca843c9f4b52e6d583ab09c11612677
RawanZaben <rawan.zaben.rae.js@tuwaiq.edu.sa>
2021-12-13 10:04:08 +0300

    Projct

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Courses from "./components/Courses";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import Navbar from "./components/Navbar";
+import { Route } from "react-router-dom";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div>
+    
+    </div>
+  );
+}
